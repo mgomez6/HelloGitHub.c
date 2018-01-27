6fe164b24759f0a00b747e6ebcc1a268c0e5ac53
Mario Gomez <35786235+mgomez6@users.noreply.github.com>
2018-01-26 19:02:56 -0600

    Add files via upload

---
+++ Hello world to sent via github.c
@@ -0,0 +1,15 @@
+/**
+Author:Aidan Johnson
+Date:1/26/2018
+This is the Hello world program to send to 
+Mario in GitHub
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char **argv)  {
+	
+	printf("Aidan Johnson and Mario Gomez");
+	return 0;
+}
